08348e6d482fe398ec7a1199a74be2d20a490632
jayvuni <167019509+jayvuni@users.noreply.github.com>
2024-05-11 10:58:32 +0100

    Added login error feature.

--- frontend/app/api/auth/login/route.js
+++ frontend/app/api/auth/login/route.js
@@ -56,6 +56,7 @@ export async function POST(request) {
     return NextResponse.json(
       {
         message: error.response.data.message,
+        error: error.response.data.error,
       },
       {
         status: error.response.status,
